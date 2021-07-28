a8ea624cc05650862ac708e3798e7b7f60ca6d50
Yaswanth3110 <69066478+Yaswanth3110@users.noreply.github.com>
2021-07-28 19:15:09 +0530

    Add files via upload

---
+++ voicecommands.js
@@ -0,0 +1,24 @@
+
+    if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'scroll (to) *tag': scroll,
+    'show (me) *tag': scroll,
+    'go (to) *tag': scroll,
+  };
+
+  function scroll(argument) {
+    x = UIkit.scroll("#home");
+    x.scrollTo("#"+argument);
+
+  }
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+}
+
+
+
+
